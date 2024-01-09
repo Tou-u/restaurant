8dcf6c4f580c2a30d375890601e5ad5b1f4208b1
Tou-u <rodrignacio99@hotmail.com>
2024-01-09 03:13:09 -0300

    db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,12 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      // host: process.env.DB_HOST,
+      // port: +process.env.DB_PORT,
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
+      url: process.env.POSTGRES_URL,
       autoLoadEntities: true,
       // ! Development only
       synchronize: true,
