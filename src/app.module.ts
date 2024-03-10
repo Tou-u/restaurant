ad8d3abc519724eddf7d88f2c57b01c18b6e6821
Tou-u <rodrignacio99@hotmail.com>
2024-03-10 00:41:30 -0300

    sincronización

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       // ! Development only
-      // synchronize: true,
+      synchronize: true,
     }),
     FoodsModule,
     CategoriesModule,
